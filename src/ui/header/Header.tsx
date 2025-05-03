e07f981da17691edcd017f1f7d059a7e6a8ba8f6
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-05-03 23:00:57 +0500

    Finished the header with ai

--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -1,21 +1,79 @@
-import Image from 'next/image'
+'use client'
+
 import Link from 'next/link'
-import { Nav } from './Nav'
+import { usePathname } from 'next/navigation'
+import { Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem } from '@/components/ui/navigation-menu'
+import Image from 'next/image'
+import clsx from 'clsx'
+
+const navLinks = [
+        { href: '/', label: 'Home' },
+        { href: '/about', label: 'About Us' },
+        { href: '/properties', label: 'Properties' },
+        { href: '/agents', label: 'Agents' },
+        { href: '/projects', label: 'Projects' },
+        { href: '/contact', label: 'Contact Us' },
+]
 
 export function Header() {
+        const pathname = usePathname()
+
         return (
-                <header className='fixed left-0 w-full'>
-                        <div className='container flex justify-between items-center py-4'>
+                <header>
+                        <div className='container flex items-center justify-between px-1 py-4 bg-white'>
                                 <Link href='/'>
-                                        <Image src='./logo.svg' alt='logo' width={173} height={30} />
+                                        <Image src='./logo.svg' alt='logo' width={173} height={30} className='max-lg:hidden' />
+                                        <Image src='./logoMin.svg' alt='logo' width={26} height={30} className='lg:hiddeb' />
                                 </Link>
-                                <Nav></Nav>
-                                <div className='flex gap-3'>
-                                        <Button className='rounded-3xl' variant='secondary'>
-                                                Sign Up
+
+                                <NavigationMenu className='hidden md:flex'>
+                                        <NavigationMenuList className='flex gap-6 text-sm font-medium text-gray-600'>
+                                                {navLinks.map(({ href, label }) => (
+                                                        <NavigationMenuItem key={href}>
+                                                                <Link href={href} className={clsx('text-base', { 'font-semibold text-black': pathname == href })}>
+                                                                        {label}
+                                                                </Link>
+                                                        </NavigationMenuItem>
+                                                ))}
+                                        </NavigationMenuList>
+                                </NavigationMenu>
+
+                                <div className='hidden md:flex gap-3'>
+                                        <Button variant='outline' className='rounded-3xl'>
+                                                <Link href='/signup'>Sign Up</Link>
                                         </Button>
-                                        <Button className='rounded-3xl'>Sign In</Button>
+                                        <Button className='rounded-3xl'>
+                                                <Link href='signin'>Sign In</Link>
+                                        </Button>
+                                </div>
+
+                                <div className='md:hidden'>
+                                        <Sheet>
+                                                <SheetTitle></SheetTitle>
+                                                <SheetTrigger asChild>
+                                                        <Button variant='ghost' size='icon'>
+                                                                <Menu className='w-6 h-6' />
+                                                        </Button>
+                                                </SheetTrigger>
+                                                <SheetContent side='right' className='w-62.5 px-10'>
+                                                        <div className='flex flex-col gap-4 mt-10 text-gray-800 font-medium'>
+                                                                {navLinks.map(({ href, label }) => (
+                                                                        <Link key={href} href={href} className={`${pathname === href ? 'text-black font-semibold' : 'hover:text-black'}`}>
+                                                                                {label}
+                                                                        </Link>
+                                                                ))}
+                                                                <div className='pt-4 border-t flex flex-col gap-2'>
+                                                                        <Button variant='outline' className='rounded-3xl'>
+                                                                                Sign In
+                                                                        </Button>
+                                                                        <Button className='rounded-3xl'>Get Started</Button>
+                                                                </div>
+                                                        </div>
+                                                </SheetContent>
+                                        </Sheet>
                                 </div>
                         </div>
                 </header>
--- src/ui/header/Nav.tsx
+++ src/ui/header/Nav.tsx
@@ -1,66 +1,52 @@
-'use client'
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu'
-import clsx from 'clsx'
-import { usePathname } from 'next/navigation'
+// 'use client'
+// import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu'
+// import clsx from 'clsx'
+// import { usePathname } from 'next/navigation'
 
-export function Nav() {
-        const pathname = usePathname()
-        return (
-                <NavigationMenu>
-                        <NavigationMenuList className='flex items-center'>
-                                <NavigationMenuItem>
-                                        <NavigationMenuLink className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/' })} href='/'>
-                                                Home
-                                        </NavigationMenuLink>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                        <NavigationMenuLink
-                                                className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/aboutus' })}
-                                                href='/aboutus'
-                                        >
-                                                About Us
-                                        </NavigationMenuLink>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem className='hover:bg-white'>
-                                        <NavigationMenuTrigger className='hover:bg-white'>
-                                                <NavigationMenuLink
-                                                        className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/props' })}
-                                                        href='/props'
-                                                >
-                                                        Properties
-                                                </NavigationMenuLink>
-                                        </NavigationMenuTrigger>
-                                        <NavigationMenuContent>
-                                                <ul className='grid w-[50px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[50px] left-1/2'>
-                                                       <li>ONe</li>
-                                                </ul>
-                                        </NavigationMenuContent>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                        <NavigationMenuLink
-                                                className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/agents' })}
-                                                href='/agents'
-                                        >
-                                                Agents
-                                        </NavigationMenuLink>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                        <NavigationMenuLink
-                                                className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/projects' })}
-                                                href='/projects'
-                                        >
-                                                Projects
-                                        </NavigationMenuLink>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                        <NavigationMenuLink
-                                                className={clsx('text-base hover:bg-white focus:bg-white text-normalGray', { 'font-semibold text-black': pathname == '/contactus' })}
-                                                href='/contactus'
-                                        >
-                                                Contact Us
-                                        </NavigationMenuLink>
-                                </NavigationMenuItem>
-                        </NavigationMenuList>
-                </NavigationMenu>
-        )
-}
+// export function Nav() {
+//         const pathname = usePathname()
+//         return (
+//                 <NavigationMenu>
+//                         <NavigationMenuList className='flex items-center'>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuLink className={clsx('text-base', { 'font-semibold text-black': pathname == '/' })} href='/'>
+//                                                 Home
+//                                         </NavigationMenuLink>
+//                                 </NavigationMenuItem>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuLink className={clsx('text-base', { 'font-semibold text-black': pathname == '/aboutus' })} href='/aboutus'>
+//                                                 About Us
+//                                         </NavigationMenuLink>
+//                                 </NavigationMenuItem>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuTrigger className=''>
+//                                                 <NavigationMenuLink
+//                                                         className={clsx('text-base hover:bg-transparent focus:bg-transparent font-normal', { 'font-semibold text-black': pathname == '/props' })}
+//                                                         href='/props'
+//                                                 >
+//                                                         Properties
+//                                                 </NavigationMenuLink>
+//                                         </NavigationMenuTrigger>
+//                                         <NavigationMenuContent>
+//                                                 <ul className='grid w-[50px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[50px] left-1/2'></ul>
+//                                         </NavigationMenuContent>
+//                                 </NavigationMenuItem>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuLink className={clsx('text-base', { 'font-semibold text-black': pathname == '/agents' })} href='/agents'>
+//                                                 Agents
+//                                         </NavigationMenuLink>
+//                                 </NavigationMenuItem>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuLink className={clsx('text-base', { 'font-semibold text-black': pathname == '/projects' })} href='/projects'>
+//                                                 Projects
+//                                         </NavigationMenuLink>
+//                                 </NavigationMenuItem>
+//                                 <NavigationMenuItem>
+//                                         <NavigationMenuLink className={clsx('text-base', { 'font-semibold text-black': pathname == '/contactus' })} href='/contactus'>
+//                                                 Contact Us
+//                                         </NavigationMenuLink>
+//                                 </NavigationMenuItem>
+//                         </NavigationMenuList>
+//                 </NavigationMenu>
+//         )
+// }
